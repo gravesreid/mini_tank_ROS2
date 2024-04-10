bd01d2e4eecfd850ccdef54fd44b771aaee90006
Reid Graves <reid@localhost.localdomain>
2024-04-10 10:01:40 -0400

    added magnetometer node

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
             'object_detection_subscriber = mini_tank_ros2.object_detection_subscriber:main',
             'image_save_subscriber = mini_tank_ros2.image_save_subscriber:main',
             'robot_object_detection_subscriber = mini_tank_ros2.robot_object_detection_subscriber:main',
+            'magnetometer_node = mini_tank_ros2.magnetometer_node:main'
         ],
     },
 )
